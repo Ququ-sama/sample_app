8461dbe02f9271f1e28572caa626178cef4a4a25
Andrey Yaglisky <notbadusername@gmail.com>
2014-02-20 01:00:42 +0200

    Настроенные Guard и Spork

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
+# Тесты для страницы Home
   describe "Home page" do
 
   	it "should have the h1 'Sample App'" do
@@ -11,11 +14,12 @@ describe "Static pages" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => " | Home")
+      page.should have_selector('title', :text => "#{base_title} | Home")
     end
   end
 
 
+# Тесты для страницы Help
 describe "Help page" do
 
 	it "should have the h1 'Help' " do
@@ -25,10 +29,12 @@ describe "Help page" do
 
 	it "should have the title 'Help' " do
 		visit '/static_pages/help'
-		page.should have_selector('title', :text => " | Help")
+		page.should have_selector('title', :text => "#{base_title} | Help")
 		end
 	end
 
+
+# Тесты для страницы About
 describe "About page" do
 
 	it "should have the h1 'About Us' " do
@@ -38,8 +44,21 @@ describe "About page" do
 
 		it "should have the title 'About Us' " do
 		visit '/static_pages/about'
-		page.should have_selector('title', :text => " | About Us")
+		page.should have_selector('title', :text => "#{base_title} | About Us")
 	end
 end
 
+# Тесты для страницы Contact
+describe "Contact page" do
+
+	it "should have the h1 'About Us" do
+		visit '/static_pages/contact'
+		page.should have_selector('h1', :text => 'Contact')
+	end
+
+	it "should have the title ' | Contact' " do
+		visit '/static_pages/contact'
+		page.should have_selector('title', :text => "#{base_title} | Contact" )
+	end
+end
 end
